38778f7547bbd60b22190a2cb82bedbfd1677f79
Igor Kravchenko <gornivv@gmail.com>
2020-05-24 00:24:15 +0300

    fix: zip

--- _dmg.sh
+++ _dmg.sh
@@ -1,5 +1,6 @@
 # test -f doppio_dev_site.dmg && rm doppio_dev_site.dmg
+mkdir "release/macos/dmg"
 echo "Start create-dmg"
 # https://github.com/sindresorhus/create-dmg
-create-dmg  'release/macos/iXn.app' 'release/macos'
+create-dmg  'release/macos/iXn.app' 'release/macos/dmg'
 echo "Finish create-dmg"
