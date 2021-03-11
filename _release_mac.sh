fee7b0b7ff4372d5974fc78bb4609ff357724cf3
Kravchenko Igor <gornivv@gmail.com>
2021-03-12 00:05:25 +0300

    up

--- _release_mac.sh
+++ _release_mac.sh
@@ -4,5 +4,6 @@ pod install
 cd ..
 flutter build macos --release
 cd macos
+echo $TEST
 export FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD=$FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD
 bundle exec fastlane rel_mac
